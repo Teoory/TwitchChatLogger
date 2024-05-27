bbb233c85dbae4f48c0b3c0f9d98ba574c537502
Berkay Köksal <berkay.koksall54@gmail.com>
2024-05-27 13:45:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3000; // Varsayılan port numarası, gerektiğinde değiştirilebilir
+const port = 3000;
 
 const messagesFolderPath = path.join(__dirname, 'Messages');
 
-// GET isteği için '/get-messages' endpoint'i
 app.get('/get-messages', (req, res) => {
   const { nickname } = req.query;
 
@@ -16,16 +15,14 @@ app.get('/get-messages', (req, res) => {
   if (fs.existsSync(userMessagesPath)) {
     const fileData = fs.readFileSync(userMessagesPath, 'utf8');
     const userMessages = JSON.parse(fileData);
-    res.json(userMessages); // JSON formatında mesajları gönder
+    res.json(userMessages); // JSON mesajları gönder
   } else {
     res.status(404).json({ error: 'Mesajlar bulunamadı' });
   }
 });
 
-// Statik dosyaları sunmak için public klasörünü kullanmak (örneğin, HTML dosyası)
 app.use(express.static('public'));
 
-// Sunucuyu başlat
 app.listen(port, () => {
   console.log(`Sunucu ${port} numaralı port üzerinde çalışıyor.`);
 });
